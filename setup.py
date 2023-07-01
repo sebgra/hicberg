2be5e470d1ea2588a2176403d679c0ed8560091d
sebgra <sebastiengradit@hotmail.com>
2023-07-01 17:31:24 +0200

    Added LICENSE, requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,81 @@
+########################################################
+########################################################
+# Title : setup.py
+# Date : 25-03-2022
+# Time : 14:22
+# Author : Sébastien Gradit
+# Github : https://github.com/sebgra
+########################################################
+########################################################
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Standalone command line tool to visualize coverage from a BAM file
+"""
+
+from setuptools import setup, find_packages
+import codecs
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Topic :: Scientific/Engineering :: Visualization",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Operating System :: MacOS",
+]
+
+name = "hicberg"
+
+MAJOR = 0
+MINOR = 1
+MAINTENANCE = 0
+VERSION = "{}.{}.{}".format(MAJOR, MINOR, MAINTENANCE)
+
+LICENSE = "MIT"
+URL = "https://github.com/sebgra/mosa-hic"
+
+DESCRIPTION = __doc__.strip("\n")
+
+with codecs.open("README.rst", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+with open("requirements.txt", "r") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+with open("hicberg/version.py", "w") as f:
+    f.write("__version__ = '{}'\n".format(VERSION))
+
+
+setup(
+    name=name,
+    author="sgradit",
+    author_email="sebastien.gradit@pasteur.fr",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    version=VERSION,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    url=URL,
+    packages=find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "hicberg=hicberg.__main__:cli",
+        ]
+    },
+)
