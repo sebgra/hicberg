03e574868c0dab38e09133b6bb3241a26b8d404f
sebgra <sebastiengradit@hotmail.com>
2023-07-04 11:44:44 +0200

    First pytest fixture test

--- hicberg/align.py
+++ hicberg/align.py
@@ -4,34 +4,6 @@ from pathlib import Path
 import subprocess as sp
 import click
 
-def hic_align(genome : str, index : str, fq_for : str, fq_rev : str, sensitivity : str = 'very_sensitive', max_alignment :  int = None, cpus : int = 1, output : str = None, verbonse : bool = False) -> None:
-    """
-    AI is creating summary for hic_align
-
-    Parameters
-    ----------
-    genome : str
-        Path to the genome file along which reads are going to be aligned.
-    index : str
-        Path to the index of the genome along which reads are going to be aligned (path to .bt2l files). Default to None, index files are searched to sample_name/data/index/sample_name.
-    fq_for : str
-        Path to .fatsa containing set of reads to align (forward mate).
-    fq_rev : str
-        Path to .fatsa containing set of reads to align (forward mate).
-    sensitivity : str, optional
-        Sensitivity of the alignment., by default 'very_sensitive'
-    max_alignment : int, optional
-        Maximum number of alignments to be returned, by default None
-    cpus : int, optional
-        Number of threads allocated for the alignment, by default None
-    output : str, optional
-        Path where the alignement files (.sam) should be stored, by default None
-    verbonse : bool, optional
-        Set weither or not the shell command should be printed, by default False
-    """    
-
-    pass
-
 
 def hic_build_index(genome : str, output : str = None , cpus : int = 1 , verbose : bool = False) -> None:
     """
@@ -72,11 +44,10 @@ def hic_build_index(genome : str, output : str = None , cpus : int = 1 , verbose
     else : 
         output_path = Path(output)
 
-        print(f"output_path : {output_path}")
         
     if not output_path.exists():
 
-        raise ValueError(f"Output path {output_path} does not exist")
+        raise ValueError(f"Output path {output_path} does not exist. Please provide existing ouput path.")
     
     sample = Path(genome).stem
     index_path = Path(output, sample)
@@ -90,6 +61,79 @@ def hic_build_index(genome : str, output : str = None , cpus : int = 1 , verbose
     sp.check_call([cmd_index], shell=True)
 
     print(f"Index built at {index_path}")
+
+
+def hic_align(genome : str, index : str, fq_for : str, fq_rev : str, sensitivity : str = 'very-sensitive', max_alignment :  int = None, cpus : int = 1, output : str = None, verbose : bool = False) -> None:
+    """
+    AI is creating summary for hic_align
+
+    Parameters
+    ----------
+    genome : str
+        Path to the genome file along which reads are going to be aligned.
+    index : str
+        Path to the index of the genome along which reads are going to be aligned (path to .bt2l files). Default to None, index files are searched to sample_name/data/index/sample_name.
+    fq_for : str
+        Path to .fatsa containing set of reads to align (forward mate).
+    fq_rev : str
+        Path to .fatsa containing set of reads to align (forward mate).
+    sensitivity : str, optional
+        Sensitivity of the alignment., by default 'very_sensitive'
+    max_alignment : int, optional
+        Maximum number of alignments to be returned, by default None
+    cpus : int, optional
+        Number of threads allocated for the alignment, by default None
+    output : str, optional
+        Path where the alignement files (.sam) should be stored, by default None
+    verbonse : bool, optional
+        Set weither or not the shell command should be printed, by default False
+    """    
+
+    fq_for_path, fq_rev_path  = Path(fq_for), Path(fq_rev)
+
+    if not fq_for_path.is_file() or not fq_rev_path.is_file():
+
+        raise IOError(f"Wront path to fastq files : {fq_for_path} or {fq_rev_path} given. \
+                    Pease provide existing files.")
+    
+    if output is None:    
+        output_path = Path(getcwd())
+    
+    else : 
+        output_path = Path(output)
+
+    if not output_path.exists():
+
+        raise ValueError(f"Output path {output_path} does not exist. Please provide existing ouput path.")
+
+    index_path = Path(output_path / index)
+
+    
+    if max_alignment is None:
+        
+        cmd_alignment_rev = f"bowtie2 --{sensitivity} -p {cpus} -a -x {index_path} -S {output_path / '1.sam'} {fq_for}"
+        cmd_alignment_for = f"bowtie2 --{sensitivity} -p {cpus} -a -x {index_path} -S {output_path / '2.sam'} {fq_rev}"
+
+    else:
+            
+        cmd_alignment_rev = f"bowtie2 --{sensitivity} -p {cpus} -k {max_alignment} {sensitivity} -p {cpus}  -x {index_path} -S {output_path / '1.sam'} {fq_rev}"
+        cmd_alignment_for = f"bowtie2 --{sensitivity} -p {cpus} -k {max_alignment} {sensitivity} -p {cpus}  -x {index_path} -S {output_path / '2.sam'} {fq_for}"
+
+    if verbose :
+
+        print(cmd_alignment_for)
+        print(cmd_alignment_rev)
+
+    
+    sp.check_call([cmd_alignment_for], shell=True)
+    sp.check_call([cmd_alignment_rev], shell=True)
+
+    print(f"Alignement done at {output_path}")
+
+
+
+
+
 
 
 def hic_view():
--- tests/test_align.py
+++ tests/test_align.py
@@ -3,20 +3,38 @@ from pathlib import Path
 import tempfile
 import hicberg.align as hal
 
-def test_hic_align():
 
-
-    pass
-
-# @pytest.fixture()
+@pytest.fixture()
 def test_hic_build_index():
 
     # temp_dir = tempfile.TemporaryDirectory()
     temp_dir_path = Path("/home/sardine/Bureau/test")
-    print(temp_dir_path)
-    hal.hic_build_index(genome = "data_test/SC288_with_micron.fa", output = temp_dir_path, verbose = True)
-    print(temp_dir_path.iterdir())
+    genome_path  = Path("data_test/SC288_with_micron.fa")
+    # print(temp_dir_path)
+    hal.hic_build_index(genome = genome_path, output = temp_dir_path, verbose = True)
+
+    # print(f"genome_path.stem : {genome_path.stem}")
+
+    # print(f"to be yielded : {temp_dir_path / genome_path.stem}")
+
+    yield temp_dir_path / genome_path.stem
+
+    # for f in temp_dir_path.iterdir():
+    #     print(f"File : {f}")
+
+    # print(temp_dir_path.iterdir())
     assert  any(temp_dir_path.iterdir()) == True
+
+def test_hic_align(test_hic_build_index):
+
+    temp_dir_path = Path("/home/sardine/Bureau/test")
+    genome_path  = Path("data_test/SC288_with_micron.fa")
+    fq_for_path = Path("data_test/forward_reads_test.fq.gz")
+    fq_rev_path = Path("data_test/reverse_reads_test.fq.gz")
+    
+    hal.hic_align(genome = genome_path, index = test_hic_build_index, fq_for = fq_for_path, fq_rev = fq_rev_path, output = temp_dir_path, verbose = True)
+
+    assert True
 
 def test_hic_view():
 
