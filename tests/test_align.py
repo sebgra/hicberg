d609a9d0dc395a6098b3a08daea71ec331f4e816
sebgra <sebastiengradit@hotmail.com>
2023-07-04 14:06:12 +0200

    Use of temporary folder for pytest fixtures

--- tests/test_align.py
+++ tests/test_align.py
@@ -1,38 +1,37 @@
 import pytest
+import os
 from pathlib import Path
 import tempfile
 import hicberg.align as hal
 
+@pytest.fixture(scope="session")
+def temporary_folder():
+    fn = tempfile.TemporaryDirectory()
+    yield fn.name
+    fn.cleanup()
 
-@pytest.fixture()
-def test_hic_build_index():
 
-    # temp_dir = tempfile.TemporaryDirectory()
-    temp_dir_path = Path("/home/sardine/Bureau/test")
+
+@pytest.fixture(name = "bowtie2_index")
+def test_hic_build_index(temporary_folder):
+
+    temp_dir_path = Path(temporary_folder)
     genome_path  = Path("data_test/SC288_with_micron.fa")
-    # print(temp_dir_path)
+    
     hal.hic_build_index(genome = genome_path, output = temp_dir_path, verbose = True)
-
-    # print(f"genome_path.stem : {genome_path.stem}")
-
-    # print(f"to be yielded : {temp_dir_path / genome_path.stem}")
 
     yield temp_dir_path / genome_path.stem
 
-    # for f in temp_dir_path.iterdir():
-    #     print(f"File : {f}")
-
-    # print(temp_dir_path.iterdir())
     assert  any(temp_dir_path.iterdir()) == True
 
-def test_hic_align(test_hic_build_index):
+def test_hic_align(bowtie2_index, temporary_folder):
 
-    temp_dir_path = Path("/home/sardine/Bureau/test")
+    temp_dir_path = Path(temporary_folder)
     genome_path  = Path("data_test/SC288_with_micron.fa")
     fq_for_path = Path("data_test/forward_reads_test.fq.gz")
     fq_rev_path = Path("data_test/reverse_reads_test.fq.gz")
-    
-    hal.hic_align(genome = genome_path, index = test_hic_build_index, fq_for = fq_for_path, fq_rev = fq_rev_path, output = temp_dir_path, verbose = True)
+
+    hal.hic_align(genome = genome_path, index = bowtie2_index, fq_for = fq_for_path, fq_rev = fq_rev_path, output = temp_dir_path, verbose = True)
 
     assert True
 
