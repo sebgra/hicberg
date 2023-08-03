b165aca1bee7db36d8a359eb3b8e4ae860615d92
sebgra <sebastiengradit@hotmail.com>
2023-08-03 17:49:40 +0200

    Raffined grid search for benchmark

--- hicberg/benchmark.py
+++ hicberg/benchmark.py
@@ -6,6 +6,8 @@ import tempfile as tmpf
 import multiprocessing as mp
 import subprocess as sp
 
+from itertools import product
+
 from shutil import rmtree
 import click
 import logging
@@ -56,20 +58,79 @@ def benchmark(output_dir : str = None, chromosome : str = "", position : int = 0
 
     print("------ Formating inputs ------")
 
-    position = [int(p) for p in position.split(",")]
-    trans_chromosome = [c for c in trans_chromosome.split(",")]
-    trans_position = [int(tp) for tp in trans_position.split(",")]
-    strides = [int(s) for s in strides.split(",")]
+    # position = [int(p) for p in position.split(",")]
+    # trans_chromosome = [c for c in trans_chromosome.split(",")]
+    # trans_position = [int(tp) for tp in trans_position.split(",")]
+    # strides = [int(s) for s in strides.split(",")]
+
+    MODE = [[m] for m in mode.split(",")]
+
+    CHROMOSOME = [c for c in chromosome.split(",")]
+
+    TRANS_CHROMOSOME = [[str(t)] for t in trans_chromosome.split(",")]
+
+    # TRANS_POSITION = [[int(p) for p in P.split(",")] for P in trans_position]
+
+    TRANS_POSITION = [[int(p)] for p in position.split(",")]
+
+
+    NB_BINS = bins
+    POSITION = [[int(p) for p in str(position).split(",")]]
+    BIN_SIZE = bins
+
+    STRIDES = [[int(s) for s in strides.split(",")]]
 
     print(f"output_dir : {output_dir}")
-    print(f"chromosome : {chromosome}")
-    print(f"position : {position}")
-    print(f"trans_chromosome : {trans_chromosome}")
-    print(f"trans_position : {trans_position}")
-    print(f"strides : {strides}")
-    print(f"mode : {mode}")
+    print(f"chromosome : {CHROMOSOME}")
+    print(f"position : {POSITION}")
+    print(f"trans_chromosome : {TRANS_CHROMOSOME}")
+    print(f"trans_position : {TRANS_POSITION}")
+    print(f"strides : {STRIDES}")
+    print(f"mode : {MODE}")
     print(f"auto : {auto}")
     print(f"bins : {bins}")
+
+    print('----- Creating grid search -----')
+
+    # stride = stride
+
+    # trans_coordinates = list(zip(*trans_chromosome, trans_position))
+    trans_coordinates = zip(TRANS_CHROMOSOME, TRANS_POSITION)
+
+    GS = list(product(MODE, CHROMOSOME, POSITION, trans_coordinates, STRIDES))
+
+    # print(list(product(MODE, CHROMOSOME, POSITION, trans_coordinates, STRIDES)))
+
+    # if len(trans_chromosome) != 0:
+
+    #     GS =  product(mode, chromosome, position, strides, trans_coordinates)
+
+    # else:
+
+    #     GS = list(product(mode, chromosome, position, strides))
+
+    # print(f"GS : {GS}")
+
+    for idx, g in enumerate(GS):
+
+        # print(f"{idx} : {g}")
+
+        print(f"GS[{idx}] : {g}")
+
+        # if len(TRANS_CHROMOSOME) != 0:
+
+    
+
+
+        #     MODE, CHROMOSOME, POSITION, STRIDE = g[:-1]
+        #     TRANS_CHROMOSOME, TRANS_POSITION = g[-1]
+
+        # # conver list of position from CLI from str to int
+        #     TRANS_POSITION = [int(x) for x in TRANS_POSITION.split(",")]
+
+        # else : 
+
+        #     MODE, CHROMOSOME, POSITION, STRIDE  =  g
 
 
 
